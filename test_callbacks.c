d7cb3a6919371ef6f07e4eb62e831b45ea9c5328
Cristian Bosin <cmaruanbosin@gmail.com>
2021-09-21 04:57:04 -0300

    unittest now uses test suites and test cases

--- test_callbacks.c
+++ test_callbacks.c
@@ -26,32 +26,35 @@ DECLARE_CALLBACK(func3);
 DECLARE_CALLBACK(func4);
 DECLARE_CALLBACK(func5);
 
-int evil_callback(void *state) {
-    int *retval = (int*) state;
-    *retval = RegisterCallback(func1, "evil-func1", NULL);
-    return 1;
+int evil_callback(void* state) {
+  int* retval = (int*)state;
+  *retval = RegisterCallback(func1, "evil-func1", NULL);
+  return 1;
 }
 
-UNITTEST_TEST(CanRegisterCallback) {
-  UNITTEST_ASSERT(CALLBACK_SUCCESS == RegisterCallback(func1, "func1", NULL));
-  ReleaseCallbacks();
+UNITTEST_TEST_SUITE_SETUP(CallbackSuite) {
+  // NOOP
 }
 
-UNITTEST_TEST(CanRegisterSameCallbackMultipleTimes) {
+UNITTEST_TEST_SUITE_TEARDOWN(CallbackSuite) { ReleaseCallbacks(); }
+
+UNITTEST_TEST_CASE(CallbackSuite, CanRegisterCallback) {
   UNITTEST_ASSERT(CALLBACK_SUCCESS == RegisterCallback(func1, "func1", NULL));
-  UNITTEST_ASSERT(CALLBACK_SUCCESS == RegisterCallback(func1, "func1", NULL));
-  ReleaseCallbacks();
 }
 
-UNITTEST_TEST(CanRegisterMultipleCallbacks) {
+UNITTEST_TEST_CASE(CallbackSuite, CanRegisterSameCallbackMultipleTimes) {
+  UNITTEST_ASSERT(CALLBACK_SUCCESS == RegisterCallback(func1, "func1", NULL));
+  UNITTEST_ASSERT(CALLBACK_SUCCESS == RegisterCallback(func1, "func1", NULL));
+}
+
+UNITTEST_TEST_CASE(CallbackSuite, CanRegisterMultipleCallbacks) {
   UNITTEST_ASSERT(CALLBACK_SUCCESS == RegisterCallback(func1, "func1", NULL));
   UNITTEST_ASSERT(CALLBACK_SUCCESS == RegisterCallback(func2, "func2", NULL));
   UNITTEST_ASSERT(CALLBACK_SUCCESS == RegisterCallback(func3, "func3", NULL));
   UNITTEST_ASSERT(CALLBACK_SUCCESS == RegisterCallback(func4, "func4", NULL));
-  ReleaseCallbacks();
 }
 
-UNITTEST_TEST(CanExecuteCallback) {
+UNITTEST_TEST_CASE(CallbackSuite, CanExecuteCallback) {
   RegisterCallback(func1, "func1", NULL);
   _CALLBACK_RETVAL(func1) = 123465;
   _CALLBACK_COUNT(func1) = 0;
@@ -59,10 +62,9 @@ UNITTEST_TEST(CanExecuteCallback) {
                    "Callback should not be counter as error");
   UNITTEST_ASSERT_(_CALLBACK_COUNT(func1) == 1,
                    "Callback should have been executed once");
-  ReleaseCallbacks();
 }
 
-UNITTEST_TEST(CanExecuteCallbackOnlyOnce) {
+UNITTEST_TEST_CASE(CallbackSuite, CanExecuteCallbackOnlyOnce) {
   RegisterCallback(func1, "func1", NULL);
   _CALLBACK_RETVAL(func1) = 123465;
   _CALLBACK_COUNT(func1) = 0;
@@ -71,89 +73,88 @@ UNITTEST_TEST(CanExecuteCallbackOnlyOnce) {
   ExecuteCallbacks(NULL);
   UNITTEST_ASSERT_(_CALLBACK_COUNT(func1) == 1,
                    "Callback should have been executed once");
-  ReleaseCallbacks();
 }
 
-UNITTEST_TEST(CanExecuteCallbackLastInFirstOut) {
+UNITTEST_TEST_CASE(CallbackSuite, CanExecuteCallbackLastInFirstOut) {
   RegisterCallback(func3, "func3", NULL);
   RegisterCallback(func2, "func2", NULL);
   RegisterCallback(func1, "func1", NULL);
   generator = 1;
   ExecuteCallbacks(NULL);
-  UNITTEST_ASSERT_(_CALLBACK_ORDER(func1) == 1, "func1 should have been called first");
-  UNITTEST_ASSERT_(_CALLBACK_ORDER(func2) == 2, "func2 should have been called second");
-  UNITTEST_ASSERT_(_CALLBACK_ORDER(func3) == 3, "func3 should have been called third");
-  ReleaseCallbacks();
+  UNITTEST_ASSERT_(_CALLBACK_ORDER(func1) == 1,
+                   "func1 should have been called first");
+  UNITTEST_ASSERT_(_CALLBACK_ORDER(func2) == 2,
+                   "func2 should have been called second");
+  UNITTEST_ASSERT_(_CALLBACK_ORDER(func3) == 3,
+                   "func3 should have been called third");
 }
 
-UNITTEST_TEST(CanRegisterCallbackWithCustomId) {
+UNITTEST_TEST_CASE(CallbackSuite, CanRegisterCallbackWithCustomId) {
   RegisterCallbackWithId(func1, "func1", NULL, 10);
   RegisterCallbackWithId(func2, "func2", NULL, 20);
   RegisterCallback(func3, "func3", NULL);
-  
+
   generator = 1;
   _CALLBACK_COUNT(func3) = 0;
 
   ExecuteCallbacksWithId(NULL, 6);
   UNITTEST_ASSERT_(generator == 1, "No callbacks with ID 6 exist");
-  
+
   ExecuteCallbacksWithId(NULL, 10);
-  UNITTEST_ASSERT_(_CALLBACK_ORDER(func1) == 1, "func1 should have been called");
-  
+  UNITTEST_ASSERT_(_CALLBACK_ORDER(func1) == 1,
+                   "func1 should have been called");
+
   ExecuteCallbacksWithId(NULL, 20);
-  UNITTEST_ASSERT_(_CALLBACK_ORDER(func2) == 2, "func2 should have been called");
-  
-  UNITTEST_ASSERT_(_CALLBACK_COUNT(func3) == 0, "func3 should not have been called");
-  ReleaseCallbacks();
+  UNITTEST_ASSERT_(_CALLBACK_ORDER(func2) == 2,
+                   "func2 should have been called");
+
+  UNITTEST_ASSERT_(_CALLBACK_COUNT(func3) == 0,
+                   "func3 should not have been called");
 }
 
-UNITTEST_TEST(CallbackReturningZeroOrNegativeCountAsFailed) {
+UNITTEST_TEST_CASE(CallbackSuite,
+                   CallbackReturningZeroOrNegativeCountAsFailed) {
   RegisterCallback(func1, "func1", NULL);
   RegisterCallback(func2, "func2", NULL);
   RegisterCallback(func3, "func3", NULL);
 
-  _CALLBACK_RETVAL(func1) = -1; // Error
-  _CALLBACK_RETVAL(func2) = 1;  // Success
-  _CALLBACK_RETVAL(func3) = 0;  // Error
-  
+  _CALLBACK_RETVAL(func1) = -1;  // Error
+  _CALLBACK_RETVAL(func2) = 1;   // Success
+  _CALLBACK_RETVAL(func3) = 0;   // Error
+
   UNITTEST_ASSERT(ExecuteCallbacks(NULL) == 2);
-  ReleaseCallbacks();
 }
 
-UNITTEST_TEST(CallbackReceiveCorrectState) {
+UNITTEST_TEST_CASE(CallbackSuite, CallbackReceiveCorrectState) {
   int a, b, c;
   RegisterCallback(func1, "func1", &a);
   RegisterCallback(func2, "func2", &b);
   RegisterCallback(func3, "func3", NULL);
-  
+
   ExecuteCallbacks(&c);
 
   UNITTEST_ASSERT(_CALLBACK_STATE(func1) == &a);
   UNITTEST_ASSERT(_CALLBACK_STATE(func2) == &b);
   UNITTEST_ASSERT(_CALLBACK_STATE(func3) == &c);
-  ReleaseCallbacks();
 }
 
-UNITTEST_TEST(CantRegisterCallbackWithinCallback) {
-    int ret;
-    RegisterCallback(evil_callback, "evil_callback", &ret);
-    _CALLBACK_COUNT(func1) = 0;
-    ExecuteCallbacks(NULL);
-    UNITTEST_ASSERT(ret == CALLBACK_LOCKED);
-    UNITTEST_ASSERT(_CALLBACK_COUNT(func1) == 0);
-    ReleaseCallbacks();
+UNITTEST_TEST_CASE(CallbackSuite, CantRegisterCallbackWithinCallback) {
+  int ret;
+  RegisterCallback(evil_callback, "evil_callback", &ret);
+  _CALLBACK_COUNT(func1) = 0;
+  ExecuteCallbacks(NULL);
+  UNITTEST_ASSERT(ret == CALLBACK_LOCKED);
+  UNITTEST_ASSERT(_CALLBACK_COUNT(func1) == 0);
 }
 
-UNITTEST_TEST(CanUnregisterCallback) {
+UNITTEST_TEST_CASE(CallbackSuite, CanUnregisterCallback) {
   RegisterCallback(func1, "func1", NULL);
   RegisterCallback(func2, "func2", NULL);
 
   UNITTEST_ASSERT(CALLBACK_SUCCESS == UnregisterCallback(func1));
-  
-  ReleaseCallbacks();
 }
 
-UNITTEST_TEST(UnregisteredCallbacksArentExecuted) {
+UNITTEST_TEST_CASE(CallbackSuite, UnregisteredCallbacksArentExecuted) {
   RegisterCallback(func1, "func1", NULL);
   RegisterCallback(func2, "func2", NULL);
 
@@ -165,21 +166,24 @@ UNITTEST_TEST(UnregisteredCallbacksArentExecuted) {
 
   UNITTEST_ASSERT(_CALLBACK_COUNT(func1) == 0);
   UNITTEST_ASSERT(_CALLBACK_COUNT(func2) == 1);
-  
-  ReleaseCallbacks();
 }
 
 UNITTEST_TESTS = {
-    UNITTEST_DECLARE(CanRegisterCallback),
-    UNITTEST_DECLARE(CanRegisterSameCallbackMultipleTimes),
-    UNITTEST_DECLARE(CanRegisterMultipleCallbacks),
-    UNITTEST_DECLARE(CanExecuteCallback),
-    UNITTEST_DECLARE(CanExecuteCallbackOnlyOnce),
-    UNITTEST_DECLARE(CanExecuteCallbackLastInFirstOut),
-    UNITTEST_DECLARE(CanRegisterCallbackWithCustomId),
-    UNITTEST_DECLARE(CallbackReturningZeroOrNegativeCountAsFailed),
-    UNITTEST_DECLARE(CallbackReceiveCorrectState),
-    UNITTEST_DECLARE(CantRegisterCallbackWithinCallback),
-    UNITTEST_DECLARE(CanUnregisterCallback),
-    UNITTEST_DECLARE(UnregisteredCallbacksArentExecuted),
-};
+    UNITTEST_DECLARE_TEST_CASE(CallbackSuite, CanRegisterCallback),
+    UNITTEST_DECLARE_TEST_CASE(CallbackSuite,
+                               CanRegisterSameCallbackMultipleTimes),
+    UNITTEST_DECLARE_TEST_CASE(CallbackSuite, CanRegisterMultipleCallbacks),
+    UNITTEST_DECLARE_TEST_CASE(CallbackSuite, CanExecuteCallback),
+    UNITTEST_DECLARE_TEST_CASE(CallbackSuite, CanExecuteCallbackOnlyOnce),
+    UNITTEST_DECLARE_TEST_CASE(CallbackSuite, CanExecuteCallbackLastInFirstOut),
+    UNITTEST_DECLARE_TEST_CASE(CallbackSuite, CanRegisterCallbackWithCustomId),
+    UNITTEST_DECLARE_TEST_CASE(CallbackSuite,
+                               CallbackReturningZeroOrNegativeCountAsFailed),
+    UNITTEST_DECLARE_TEST_CASE(CallbackSuite, CallbackReceiveCorrectState),
+    UNITTEST_DECLARE_TEST_CASE(CallbackSuite,
+                               CantRegisterCallbackWithinCallback),
+    UNITTEST_DECLARE_TEST_CASE(CallbackSuite, CanUnregisterCallback),
+    UNITTEST_DECLARE_TEST_CASE(CallbackSuite,
+                               UnregisteredCallbacksArentExecuted),
+
+    UNITTEST_END};
--- unittest.h
+++ unittest.h
@@ -3,8 +3,8 @@
 
 #include <stddef.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct {
   const char *name;
@@ -14,8 +14,8 @@ typedef struct {
   void *state;
 } unittest_test_t;
 
-static int UnittestEvaluate(const char *file, int line, int level, const char *expr,
-                     int result, const char *msg);
+static int UnittestEvaluate(const char *file, int line, int level,
+                            const char *expr, int result, const char *msg);
 static void UnittestInit(int argc, char const *argv[]);
 static int UnittestRun(unittest_test_t tests[]);
 
@@ -26,34 +26,49 @@ static int UnittestRun(unittest_test_t tests[]);
   UnittestEvaluate(__FILE__, __LINE__, UNITTEST_LEVEL_CHECK, #x, x, NULL)
 #define UNITTEST_CHECK_(x, msg) \
   UnittestEvaluate(__FILE__, __LINE__, UNITTEST_LEVEL_CHECK, #x, x, msg)
-#define UNITTEST_ASSERT(x)                                                \
+#define UNITTEST_ASSERT(x)                                                  \
   if (UnittestEvaluate(__FILE__, __LINE__, UNITTEST_LEVEL_ASSERTION, #x, x, \
-                       NULL) > 0)                                         \
+                       NULL) > 0)                                           \
     return;
-#define UNITTEST_ASSERT_(x, msg)                                          \
+#define UNITTEST_ASSERT_(x, msg)                                            \
   if (UnittestEvaluate(__FILE__, __LINE__, UNITTEST_LEVEL_ASSERTION, #x, x, \
-                       msg) > 0)                                          \
+                       msg) > 0)                                            \
     return;
 
 #define UNITTEST_TEST(x) void x(void)
 
-#define UNITTEST_TESTS unittest_test_t test_list[] 
+#define UNITTEST_TEST_SUITE_SETUP(suite) void UnittestSuiteSetup__##suite(void)
+
+#define UNITTEST_TEST_SUITE_TEARDOWN(suite) \
+  void UnittestSuiteTearDown__##suite(void)
+
+#define UNITTEST_TEST_CASE(suite, test)               \
+  UNITTEST_TEST(UnittestSuitTestCase__##suite##test); \
+  UNITTEST_TEST(UnitTestWrapper__##suite##test) {     \
+    (UnittestSuiteSetup__##suite)();                  \
+    (UnittestSuitTestCase__##suite##test)();          \
+    (UnittestSuiteTearDown__##suite)();               \
+  }                                                   \
+  UNITTEST_TEST(UnittestSuitTestCase__##suite##test)
+
+#define UNITTEST_TESTS unittest_test_t test_list[]
 
 #define UNITTEST_DECLARE(x) \
   { #x, x, 0, 0, NULL }
 
-#define UNITTEST_END {NULL, NULL, 0, 0, NULL }
+#define UNITTEST_DECLARE_TEST_CASE(suite, test) \
+  { #suite "::" #test, UnitTestWrapper__##suite##test, 0, 0, NULL }
 
+#define UNITTEST_END \
+  { NULL, NULL, 0, 0, NULL }
 
 #if !defined(UNITTEST_DONT_USE_DEFAULT_MAIN)
 extern unittest_test_t test_list[];
-int main(int argc, char const *argv[])
-{
-    UnittestInit(argc, argv);
-    return UnittestRun(test_list);
+int main(int argc, char const *argv[]) {
+  UnittestInit(argc, argv);
+  return UnittestRun(test_list);
 }
-#endif // UNITTEST_USE_DEFAULT_MAIN
-
+#endif  // UNITTEST_USE_DEFAULT_MAIN
 
 static int failed_assertions;
 static int failed_checks;
@@ -62,129 +77,116 @@ static int total_checks;
 static int verbosity = 1;
 static unittest_test_t *current_test;
 
-
-
 void ResetState() {
-    failed_assertions = 0;
-    failed_checks = 0;
-    current_test = NULL;
+  failed_assertions = 0;
+  failed_checks = 0;
+  current_test = NULL;
 }
 
+static void UnittestInit(int argc, char const *argv[]) {}
 
-static void UnittestInit(int argc, char const *argv[])
-{
-
-}
-
-static void ShowTestInfoShort(const unittest_test_t *test)
-{
-    static const char padding[] = \
-        ".....................................................................";
-    int pad_length = sizeof(padding) - strlen(test->name);
-    if (pad_length < 0)
-        pad_length = 0;
-    fprintf(stderr, "%s %*.*s", test->name, pad_length, pad_length, padding);
-    if (test->failed_assertions > 0 || test->failed_checks > 0) {
-        fprintf(stderr, " FAILED\n");
-    } else {
-        fprintf(stderr, " OK\n");
-    }
+static void ShowTestInfoShort(const unittest_test_t *test) {
+  static const char padding[] =
+      ".....................................................................";
+  int pad_length = sizeof(padding) - strlen(test->name);
+  if (pad_length < 0) pad_length = 0;
+  fprintf(stderr, "%s %*.*s", test->name, pad_length, pad_length, padding);
+  if (test->failed_assertions > 0 || test->failed_checks > 0) {
+    fprintf(stderr, " FAILED\n");
+  } else {
+    fprintf(stderr, " OK\n");
+  }
 }
 
 static int RunTest(unittest_test_t *test) {
-    ResetState();
-    current_test = test;
-    test->test();
-    test->failed_assertions = failed_assertions;
-    test->failed_checks = failed_checks;
-    if (verbosity > 0)
-        ShowTestInfoShort(test);
-    return failed_assertions > 0 || failed_checks > 0;
+  ResetState();
+  current_test = test;
+  test->test();
+  test->failed_assertions = failed_assertions;
+  test->failed_checks = failed_checks;
+  if (verbosity > 0) ShowTestInfoShort(test);
+  return failed_assertions > 0 || failed_checks > 0;
 }
 
-static int UnittestRun(unittest_test_t tests[])
-{
-    int i;
-    int count = 0;
-    int failed = 0;
-    int _failed_assertions = 0;
-    int _failed_checks = 0;
-    unittest_test_t *test = tests;
-    while (test->test != NULL) {
-        failed += RunTest(test++);
-        _failed_assertions += failed_assertions;
-        _failed_checks += failed_checks;
-        count++;
+static int UnittestRun(unittest_test_t tests[]) {
+  int i;
+  int count = 0;
+  int failed = 0;
+  int _failed_assertions = 0;
+  int _failed_checks = 0;
+  unittest_test_t *test = tests;
+  while (test->test != NULL) {
+    failed += RunTest(test++);
+    _failed_assertions += failed_assertions;
+    _failed_checks += failed_checks;
+    count++;
+  }
+  test = tests;
+  while (test->test != NULL) {
+    if (test->failed_assertions > 0 || test->failed_checks > 0) {
+      fprintf(stderr,
+              "----------------------------------------------------------------"
+              "-------\n");
+      fprintf(stderr, "Test `%s` FAILED\n", test->name);
+      fprintf(stderr, "%s\n", (const char *)test->state);
     }
-    test = tests;
-    while (test->test != NULL) {
-        if (test->failed_assertions > 0 || test->failed_checks > 0) {
-            fprintf(stderr, "-----------------------------------------------------------------------\n");
-            fprintf(stderr, "Test `%s` FAILED\n", test->name);
-            fprintf(stderr, "%s\n", (const char*)test->state);
-        }
-        free(test->state);
-        test++;
-    }
-    fprintf(stderr, "-----------------------------------------------------------------------\n");
-    fprintf(stderr, "Executed %d tests, %d failed\n", count, failed);
-    if (failed > 0) {
-        fprintf(stderr, "Failed assertions: %d\n", _failed_assertions);
-        fprintf(stderr, "Failed checks: %d\n", _failed_checks);
-    }
-    return failed;
+    free(test->state);
+    test++;
+  }
+  fprintf(stderr,
+          "--------------------------------------------------------------------"
+          "---\n");
+  fprintf(stderr, "Executed %d tests, %d failed\n", count, failed);
+  if (failed > 0) {
+    fprintf(stderr, "Failed assertions: %d\n", _failed_assertions);
+    fprintf(stderr, "Failed checks: %d\n", _failed_checks);
+  }
+  return failed;
 }
 
-static char * AppendString(char *a, const char *b)
-{
-    size_t sz_a, sz_b;
-    int offset = 0;
-    sz_b = strlen(b);
-    if (a == NULL) {
-        sz_a = 0;
-    } else {
-        sz_a = strlen(a);
-    }
-    char *str = malloc(sz_a + sz_b + 2);
-    
-    if (sz_a != 0)
-        offset += sprintf(str + offset, "%s\n", a);
-    
-    if (sz_b != 0)
-        offset += sprintf(str + offset, "%s", b);
-    
-    if (a != NULL)
-        free(a);
+static char *AppendString(char *a, const char *b) {
+  size_t sz_a, sz_b;
+  int offset = 0;
+  sz_b = strlen(b);
+  if (a == NULL) {
+    sz_a = 0;
+  } else {
+    sz_a = strlen(a);
+  }
+  char *str = malloc(sz_a + sz_b + 2);
 
-    return str;
+  if (sz_a != 0) offset += sprintf(str + offset, "%s\n", a);
+
+  if (sz_b != 0) offset += sprintf(str + offset, "%s", b);
+
+  if (a != NULL) free(a);
+
+  return str;
 }
 
-static int UnittestEvaluate(const char *file, int line, int level, const char *expr, int result, const char *msg)
-{
-    static char buff[4096];
-    int sz = 0;
-    int offset = 0;
-    const char *level_name;
-    if (!result) {
-        sz = sprintf(buff + sz, "[%s:%d] ", file, line);
-        offset = sz;
-        if (level == UNITTEST_LEVEL_ASSERTION) {
-            failed_assertions++;
-            sz += sprintf(buff + sz, "ASSERTION FAILED: ");
-        } else if (level == UNITTEST_LEVEL_CHECK) {
-            failed_checks++;
-            sz += sprintf(buff + sz, "CHECK FAILED: ");
-        }
-        
-        sz += sprintf(buff + sz, "`%s` is false.", expr);
-        if (msg) 
-            sz += sprintf(buff + sz - 1, "\n%*.*s%s", offset, offset, "", msg);
-        
-        current_test->state = AppendString((char*)current_test->state, buff);
+static int UnittestEvaluate(const char *file, int line, int level,
+                            const char *expr, int result, const char *msg) {
+  static char buff[4096];
+  int sz = 0;
+  int offset = 0;
+  const char *level_name;
+  if (!result) {
+    sz = sprintf(buff + sz, "[%s:%d] ", file, line);
+    offset = sz;
+    if (level == UNITTEST_LEVEL_ASSERTION) {
+      failed_assertions++;
+      sz += sprintf(buff + sz, "ASSERTION FAILED: ");
+    } else if (level == UNITTEST_LEVEL_CHECK) {
+      failed_checks++;
+      sz += sprintf(buff + sz, "CHECK FAILED: ");
     }
-    return failed_assertions > 0 || failed_checks > 0;
+
+    sz += sprintf(buff + sz, "`%s` is false.", expr);
+    if (msg) sz += sprintf(buff + sz - 1, "\n%*.*s%s", offset, offset, "", msg);
+
+    current_test->state = AppendString((char *)current_test->state, buff);
+  }
+  return failed_assertions > 0 || failed_checks > 0;
 }
-
-
 
 #endif  // UNITTEST_H
